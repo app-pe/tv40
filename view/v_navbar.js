a1d2c67797be781b0bcfdf242925b5f2273a547f
app-pe <54180318+app-pe@users.noreply.github.com>
2025-05-30 09:20:25 -0500

    Update v_navbar.js

--- view/v_navbar.js
+++ view/v_navbar.js
@@ -63,7 +63,7 @@ let navbar =
     <div class="col-lg-12">
         <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
             <a href="" class="text-decoration-none d-block d-lg-none">
-                <h1 class="m-0 display-5 font-weight-semi-bold"><span class="text-primary font-weight-bold border px-3 mr-1">TV</span>40</h1>
+                <h1 class="m-0 display-5 font-weight-semi-bold"><span class="text-primary font-weight-bold border px-3 mr-1">TV40</span>Shop</h1>
             </a>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse2">
                 <span class="navbar-toggler-icon"></span>
