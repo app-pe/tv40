5d979b15272ddaa86e428ec2b2725faee65cbd76
Raiko <mbolivar.ing@gmail.com>
2025-07-10 12:02:35 -0500

    update link a index en navbar

--- view/v_navbar.js
+++ view/v_navbar.js
@@ -62,7 +62,7 @@ let navbar =
 <div class="row border-top px-xl-5">  
     <div class="col-lg-12">
         <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
-            <a href="" class="text-decoration-none d-block d-lg-none">
+            <a href="https://app-pe.github.io/tv40/index.html" class="text-decoration-none d-block d-lg-none">
                 <h1 class="m-0 display-5 font-weight-semi-bold"><span class="text-primary font-weight-bold border px-3 mr-1">TV40</span>Shop</h1>
             </a>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse2">
