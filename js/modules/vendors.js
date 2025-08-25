98c3613a5de808a5f902162c39fbf3a6a429d488
Raiko <mbolivar.ing@gmail.com>
2025-08-25 09:09:55 -0500

    update shop vendors

--- js/modules/vendors.js
+++ js/modules/vendors.js
@@ -17,7 +17,13 @@ let vendors =
             </div> 
             <div class="vendor-item border p-4">
             <a href="https://app-pe.github.io/tv40/shop.html?id=shop7"><img src="img/vendor-4.jpg" alt=""></a>                
-            </div>            
+            </div>
+            <div class="vendor-item border p-4">
+            <a href="https://app-pe.github.io/tv40/shop.html?id=shop8"><img src="img/vendor-5.jpg" alt=""></a>                
+            </div>
+            <div class="vendor-item border p-4">
+            <a href="https://app-pe.github.io/tv40/shop.html?id=shop1"><img src="img/vendor-6.jpg" alt=""></a>                
+            </div>                
         </div>
     </div>
 </div>`;
