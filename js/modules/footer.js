d056b44c716af3bcaf0e6c24902bfc8caf9c9f68
Raiko <mbolivar.ing@gmail.com>
2025-08-25 13:23:19 -0500

    update imagen categoria cafe

--- js/modules/footer.js
+++ js/modules/footer.js
@@ -2,7 +2,7 @@ let footer =
 `
 <div class="row px-xl-5 pt-5">
     <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
-        <a href="" class="text-decoration-none">
+        <a href="https://app-pe.github.io/tv40/index.html" class="text-decoration-none">
             <h1 class="mb-4 display-5 font-weight-semi-bold"><span class="text-primary font-weight-bold border border-white px-3 mr-1">TV40</span>Shop</h1>
         </a>
         <p>Es una plataforma virtual con propósito de servicio al emprendedor, mostramos tu catálogo de productos por un periodo gratuito de 40 días.</p>        
@@ -23,7 +23,7 @@ let footer =
  <div class="row border-top border-light mx-xl-5 py-4">
      <div class="col-md-6 px-xl-0">
          <p class="mb-md-0 text-center text-md-left text-dark">
-             &copy; <a class="text-dark font-weight-semi-bold" href="#">TV40</a>. All Rights Reserved.
+             &copy; <a class="text-dark font-weight-semi-bold" href="https://app-pe.github.io/tv40/index.html">TV40</a>. All Rights Reserved.
          </p>
      </div>
      <div class="col-md-6 px-xl-0 text-center text-md-right">
