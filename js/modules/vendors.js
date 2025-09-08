20f9a6d069645d4142c8bfa866b0cb704c6e622e
Mario <mbolivar.ing@gmail.com>
2025-09-08 18:09:47 -0500

    update vendors shop 10 y 11

--- js/modules/vendors.js
+++ js/modules/vendors.js
@@ -7,6 +7,12 @@ let vendors =
     <div class="col">
         <div class="owl-carousel vendor-carousel">
             <div class="vendor-item border p-4">
+            <a href="https://app-pe.github.io/tv40/shop.html?id=shop10"><img src="img/vendor-10.jpg" alt=""></a>                
+            </div>
+            <div class="vendor-item border p-4">
+            <a href="https://app-pe.github.io/tv40/shop.html?id=shop11"><img src="img/vendor-11.jpg" alt=""></a>                
+            </div>
+            <div class="vendor-item border p-4">
             <a href="https://app-pe.github.io/tv40/shop.html?id=shop4"><img src="img/vendor-1.jpg" alt=""></a>                
             </div>
             <div class="vendor-item border p-4">
@@ -26,7 +32,7 @@ let vendors =
             </div>   
             <div class="vendor-item border p-4">
             <a href="https://app-pe.github.io/tv40/shop.html?id=shop9"><img src="img/vendor-7.jpeg" alt=""></a>                
-            </div>             
+            </div>                         
         </div>
     </div>
 </div>`;
