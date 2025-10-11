3e868b6db2c609cab4aca26ca470edd591a835d6
Raiko <mbolivar.ing@gmail.com>
2025-10-11 08:15:25 -0500

    update url shop vendors

--- js/modules/vendors.js
+++ js/modules/vendors.js
@@ -7,31 +7,31 @@ let vendors =
     <div class="col">
         <div class="owl-carousel vendor-carousel">
             <div class="vendor-item border p-4">
-            <a href="https://app-pe.github.io/tv40/shop.html?id=shop10"><img src="img/vendor-10.jpg" alt=""></a>                
+            <a href="shop.html?id=shop10"><img src="img/vendor-10.jpg" alt=""></a>                
             </div>
             <div class="vendor-item border p-4">
-            <a href="https://app-pe.github.io/tv40/shop.html?id=shop11"><img src="img/vendor-11.jpg" alt=""></a>                
+            <a href="shop.html?id=shop11"><img src="img/vendor-11.jpg" alt=""></a>                
             </div>
             <div class="vendor-item border p-4">
-            <a href="https://app-pe.github.io/tv40/shop.html?id=shop4"><img src="img/vendor-1.jpg" alt=""></a>                
+            <a href="shop.html?id=shop4"><img src="img/vendor-1.jpg" alt=""></a>                
             </div>
             <div class="vendor-item border p-4">
             <a href="shop.html?id=shop5"><img src="img/vendor-2.jpg" alt=""></a>                
             </div>
             <div class="vendor-item border p-4">
-            <a href="https://app-pe.github.io/tv40/shop.html?id=shop6"><img src="img/vendor-3.jpg" alt=""></a>                
+            <a href="shop.html?id=shop6"><img src="img/vendor-3.jpg" alt=""></a>                
             </div> 
             <div class="vendor-item border p-4">
-            <a href="https://app-pe.github.io/tv40/shop.html?id=shop7"><img src="img/vendor-4.jpg" alt=""></a>                
+            <a href="shop.html?id=shop7"><img src="img/vendor-4.jpg" alt=""></a>                
             </div>
             <div class="vendor-item border p-4">
-            <a href="https://app-pe.github.io/tv40/shop.html?id=shop8"><img src="img/vendor-5.jpg" alt=""></a>                
+            <a href="shop.html?id=shop8"><img src="img/vendor-5.jpg" alt=""></a>                
             </div>
             <div class="vendor-item border p-4">
-            <a href="https://app-pe.github.io/tv40/shop.html?id=shop1"><img src="img/vendor-6.jpg" alt=""></a>                
+            <a href="shop.html?id=shop1"><img src="img/vendor-6.jpg" alt=""></a>                
             </div>   
             <div class="vendor-item border p-4">
-            <a href="https://app-pe.github.io/tv40/shop.html?id=shop9"><img src="img/vendor-7.jpeg" alt=""></a>                
+            <a href="shop.html?id=shop9"><img src="img/vendor-7.jpeg" alt=""></a>                
             </div>                         
         </div>
     </div>
